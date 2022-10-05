eb46627910c77bc9e5ff93097db43db72fa40142
Aakankshi2204 <115091812+Aakankshi2204@users.noreply.github.com>
2022-10-05 20:07:14 +0530

    Aakankshi3.cpp

---
+++ Aakankshi3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float a,r;
+    cout<<"enter the number";
+    cin>>a;
+    r=cbrt(a);
+    cout<<"cube root of "<<a<<" is "<<r;
+    return 0;
+}
