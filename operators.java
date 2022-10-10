929bffd81f807a927a5343794b85715d65ef5c33
AshishRaghav009 <95585539+AshishRaghav009@users.noreply.github.com>
2022-10-10 22:58:52 +0530

    Create operators.java

---
+++ operators.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    
+    // declare variables
+    int a = 12, b = 5;
+
+    // addition operator
+    System.out.println("a + b = " + (a + b));
+
+    // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
+    // modulo operator
+    System.out.println("a % b = " + (a % b));
+  }
+}
