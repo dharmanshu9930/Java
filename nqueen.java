6a0129e010bcb7fa55383e3a2672afbf46649253
Riya-2003 <92114998+Riya-2003@users.noreply.github.com>
2022-10-11 21:40:18 +0530

    Add files via upload

---
+++ nqueen.java
@@ -0,0 +1,23 @@
+public class nqueen  {
+//      public static void main(String[] args){  
+//           for (int i = 0; i <= 5; i++){  
+//           int alphabet = 65;  
+//           for (int j = 5; j > i; j--){  
+//           System.out.print(" ");  
+//           }  
+//           for (int k = 0; k <= i; k++){  
+//           System.out.print((char) (alphabet + k) + " ");  
+// }  
+//           System.out.println();  
+// }  
+// }  
+// }  
+public static void main(String[] args){
+     for (int count = 1; count <= 10; count++) 
+     for (int count = 1; count < 10; count++) 
+     for (int count = 0; count < 10; count++) 
+     for (int count = 10; count > 10; count--) 
+     System.out.println(“BML University”); 
+     } 
+     } 
+     
