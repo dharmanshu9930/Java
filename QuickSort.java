dce39975b8a8eeca1aa844440257024c0456cc4a
sidduputchala <97608492+sidduputchala@users.noreply.github.com>
2022-10-14 16:27:13 +0530

    added quick sort algorithm in java

--- QuickSort.java
+++ QuickSort.java
@@ -1,73 +1,44 @@
-import java.util.Arrays;
+public class QuickSort {
+    static int ar[] = { 10, 7, 8, 9, 1, 5 };
 
-class Quicksort {
-
-  // method to find the partition position
-  static int partition(int array[], int low, int high) {
-    
-    // choose the rightmost element as pivot
-    int pivot = array[high];
-    
-    // pointer for greater element
-    int i = (low - 1);
-
-    // traverse through all elements
-    // compare each element with pivot
-    for (int j = low; j < high; j++) {
-      if (array[j] <= pivot) {
-
-        // if element smaller than pivot is found
-        // swap it with the greater element pointed by i
-        i++;
-
-        // swapping element at i with element at j
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-
+    public static void quicksort(int l, int r) {
+        if (l < r) {
+            int mid = partition(l, r);
+            quicksort(l, mid - 1);
+            quicksort(mid + 1, r);
+        }
     }
 
-    // swapt the pivot element with the greater element specified by i
-    int temp = array[i + 1];
-    array[i + 1] = array[high];
-    array[high] = temp;
+    public static int partition(int l, int r) {
+        int pivot = ar[r];
+        int i = (l - 1);
 
-    // return the position from where partition is done
-    return (i + 1);
-  }
-
-  static void quickSort(int array[], int low, int high) {
-    if (low < high) {
-
-      // find pivot element such that
-      // elements smaller than pivot are on the left
-      // elements greater than pivot are on the right
-      int pi = partition(array, low, high);
-      
-      // recursive call on the left of pivot
-      quickSort(array, low, pi - 1);
-
-      // recursive call on the right of pivot
-      quickSort(array, pi + 1, high);
+        for (int j = l; j <= r - 1; j++) {
+            if (ar[j] < pivot) {
+                i++;
+                int temp = ar[i];
+                ar[i] = ar[j];
+                ar[j] = temp;
+            }
+        }
+        int temp = ar[i + 1];
+        ar[i + 1] = ar[r];
+        ar[r] = temp;
+        return (i + 1);
     }
-  }
-}
 
-// Main class
-class Main {
-  public static void main(String args[]) {
+    public static void main(String[] args) {
+        System.out.println("Before Sorting");
+        for (Integer integer : ar) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+        quicksort(0, ar.length - 1);
+        System.out.println("After Sorting");
+        for (Integer integer : ar) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+    }
 
-    int[] data = { 8, 7, 2, 1, 0, 9, 6 };
-    System.out.println("Unsorted Array");
-    System.out.println(Arrays.toString(data));
-
-    int size = data.length;
-
-    // call quicksort() on array data
-    Quicksort.quickSort(data, 0, size - 1);
-
-    System.out.println("Sorted Array in Ascending Order ");
-    System.out.println(Arrays.toString(data));
-  }
 }
