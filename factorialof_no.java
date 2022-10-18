b3d747180a19ca021f46d9ada1470ef982cd741a
devyani-brd <116087749+devyani-brd@users.noreply.github.com>
2022-10-18 19:06:08 +0530

    Create factorialof_no.java

---
+++ factorialof_no.java
@@ -0,0 +1,18 @@
+class Test
+{
+ // Method to find factorial of given number
+ static int factorial(int n)
+ {
+  int res = 1, i;
+  for (i=2; i<=n; i++)
+   res *= i;
+  return res;
+ }
+  
+ // Driver method
+ public static void main(String[] args)
+ {
+  int num = 5;
+  System.out.println("Factorial of "+ num + " is " + factorial(5));
+ }
+}
